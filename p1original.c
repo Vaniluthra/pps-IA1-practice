ff52364572ee96cf5cd0a736e74096fc830d1f29
vaniluthra <94587714+Vaniluthra@users.noreply.github.com>
2022-01-30 13:25:25 +0000

    p1

--- p1original.c
+++ p1original.c
@@ -1,2 +1,26 @@
+#include <stdio.h>
 
+int input(int *a, int *b)
+{
+  printf("Enter number: ");
+  scanf("%d%d",a,b);
+}
+
+int add(int a,int b,int *c)
+{
+  *c=a+b;
+}
+
+void output(int a,int b,int c)
+{
+  printf("Sum of %d and %d is: %d\n",a,b,c);
+}
+
+int main()
+{
+  int x,y,z;
+  input(&x,&y);
+  add(x,y,&z);
+  output(x,y,z);
+  return 0;
 }
